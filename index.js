6c871e6f9378d487eddf8b86da48aeeaeff0525b
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-17 13:27:56 -0700

    Implemented Resizing Feature Seperated seat grid function to a seperate js file Added more attributed to input fields for rows and cols in html file Added event listener to rows and cols submission button Every time submission button is pressed the current children are removed and the new grid is formed.

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-let table = document.querySelector(".seat-grid");
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let numRows = document.querySelector('#rows');
+let numCols = document.querySelector('#cols');
+let submitSize = document.getElementById('table-adjust');
 
-
-function createTable(numRows, numCols) {
-    let topRow = document.createElement('tr');
-    topRow.classList.add('row');
-
-    /* We must add an empty cell at the beginning to account for 
-        row labels */
-    let emptyCell = document.createElement('th');
-    emptyCell.classList.add('cell');
-    emptyCell.classList.add('first-col');
-    topRow.appendChild(emptyCell);
-
-    for (let i = 1; i <= numCols; i++) {
-        let columnLabel = document.createElement('th');
-        columnLabel.textContent = `${i}`;
-        columnLabel.classList.add('cell');
-        topRow.appendChild(columnLabel);
+submitSize.addEventListener('click', () => {
+    if (numRows.value <= 0 || numRows.value > 52 || numCols.value <= 0 || numCols.value > 200) {
+        return;
     }
 
-    table.appendChild(topRow);
-
-    for (let i = numRows - 1; i >= 0; i--) {
-        const row = document.createElement('tr');
-        row.classList.add('row');
-        row.style.height = `5rem`;
-
-        let rowLabel = document.createElement('th');
-        rowLabel.textContent = `${letters[i]}`;
-        rowLabel.classList.add('cell');
-        rowLabel.classList.add('first-col');
-        row.appendChild(rowLabel);
-
-        for (let j = 0; j < numCols; j++) {
-            const cell = document.createElement('td');
-            // provides hover effect for pixel
-            cell.classList.add('cell');
-            cell.textContent = `${letters[i]}-${j + 1}`
-            cell.setAttribute("tabindex", '0')
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+    let table = document.querySelector('.seat-grid');
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
     }
-} 
+
+    console.log(numRows.value)
+    console.log(numCols.value)
+    createTable(numRows.value, numCols.value);
+});
 
 createTable(40, 100);
