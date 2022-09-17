0f477dcfef1bf2a92f606f154a337ad23e9c7d83
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-17 08:40:23 -0700

    Table Implementation Added a JS-rendered table which show the coordinate of the seat. Drawing function currently has hardcoded size and styling has not been completed.

---
+++ index.js
@@ -0,0 +1,27 @@
+let table = document.querySelector(".seat-grid");
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+function createTable(numRows, numCols) {
+    for (let i = numRows - 1; i >= 0; i--) {
+        const row = document.createElement('tr');
+        row.classList.add('row');
+        row.style.height = `5rem`
+
+        for (let j = 0; j < numCols; j++) {
+            const cell = document.createElement('td');
+            // provides hover effect for pixel
+            cell.classList.add('cell');
+            cell.style.width = `10rem`;
+            cell.style.height = `20px`;
+            cell.textContent = `${letters[i]}-${j + 1}`
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+} 
+
+createTable(40, 100);
