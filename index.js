afb7bdc4d4e07d5a3984e4bf668b21619c50fb65
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-17 11:23:45 -0700

    Added Tabel Labels Added column and row labels to the table

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 
 function createTable(numRows, numCols) {
+    let topRow = document.createElement('tr');
+    topRow.classList.add('row');
+
+    for (let i = 0; i < numCols; i++) {
+        let columnLabel = document.createElement('th');
+        columnLabel.textContent = `${i}`;
+        columnLabel.classList.add('cell');
+        topRow.appendChild(columnLabel);
+    }
+
+    table.appendChild(topRow);
+
     for (let i = numRows - 1; i >= 0; i--) {
         const row = document.createElement('tr');
         row.classList.add('row');
-        row.style.height = `5rem`
+        row.style.height = `5rem`;
+
+        let rowLabel = document.createElement('th');
+        rowLabel.textContent = `${letters[i]}`;
+        rowLabel.classList.add('cell');
+        row.appendChild(rowLabel);
 
         for (let j = 0; j < numCols; j++) {
             const cell = document.createElement('td');
