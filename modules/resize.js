4cebe918cbefe8b748f4de4601bc3e153cc56c72
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-18 17:35:56 -0700

    Added confirmation box for resizing + comments Added confirmation box browser popup, lets user know that resizing the table will clear all current values. Added more + better comments.

--- modules/resize.js
+++ modules/resize.js
@@ -1,20 +1,31 @@
+/* input boxes with user-specified sizes */
 let numRows = document.querySelector('#rows');
 let numCols = document.querySelector('#cols');
-let submitSize = document.getElementById('table-adjust');
+
+/* submit button */
+let submitSize = document.querySelector('#table-adjust');
 
 submitSize.addEventListener('click', () => {
+    /* If given size exceeds the value limits we have then don't process resize */
     if (numRows.value <= 0 || numRows.value > 52 || numCols.value <= 0 || numCols.value > 200) {
         return;
     }
 
+    /* make sure the user knows that all their current inputted values
+            will be cleared */
+    let confirm = window.confirm("Current table values will be cleared,\nAre you sure you want to continue?");
+    if (!confirm) {
+        return;
+    }
+
+    /* delete all the seats that are currently in the grid */
     let table = document.querySelector('.seat-grid');
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
 
-    console.log(numRows.value)
-    console.log(numCols.value)
     createTable(numRows.value, numCols.value);
 });
 
-createTable(40, 100);
+/* Initialize table at 10x10 (arbitrarily chosen size) */
+createTable(10, 10);
