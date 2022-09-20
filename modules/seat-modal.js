eefb951d9432a75de128f1d08e9be138be557515
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-20 10:08:28 -0700

    This file goes with prev commit

--- modules/seat-modal.js
+++ modules/seat-modal.js
@@ -1,16 +1,33 @@
-
-function createModal(name, email, level) {
+function createModal(modal_title, row, column) {
     let modal = document.createElement('div'); modal.classList.add('modal');
     let content = document.createElement('div'); content.classList.add('modal-content');
 
     let header = document.createElement('div'); header.classList.add('modal-header');
-    let close_btn = document.createElement('span'); close_btn.classList.add = 'close';
-        close_btn.textContent = '&times;';
+    let close_btn = document.createElement('span'); close_btn.classList.add("close");
+        close_btn.textContent = '\u00D7';
+
+    /* let left_arrow = document.createElement('i');
+        left_arrow.classList.add("fa-solid");
+        left_arrow.classList.add("fa-arrow-left");
+    let right_arrow = document.createElement('i');
+        right_arrow.classList.add("fa-solid");
+        right_arrow.classList.add("fa-arrow-right"); */
+
+
     let title = document.createElement('h2');
-        title.textContent = 'Student';
+        title.innerHTML = `
+        <i class="fa-solid fa-arrow-left arrow-nav" style="color: black;
+                                                margin-right: 2em;"></i>
+        ${modal_title}
+        <i class="fa-solid fa-arrow-right arrow-nav" style="color: black;
+                                                        margin-left: 2em;"></i>
+        `;
+
     header.appendChild(close_btn); header.appendChild(title);
 
-    let body = createForm(name, email, level);
+    let student = SEAT_GRID[row][column];
+    console.log(student);
+    let body = createForm(student.name, student.email, student.level);
 
     let footer = document.createElement('div'); footer.classList.add('modal-footer');
 
@@ -24,41 +41,45 @@ function createModal(name, email, level) {
 
 function createForm(name, email, level) {
     let body = document.createElement('div'); body.classList.add('modal-body');
-    let categories = ['name', 'email'];
-    let line_break = document.createElement('br');
-    body.appendChild(label);
-    for (let i = 0; i < 2; i++) {
+    body.appendChild(document.createElement('br'));
+
         let label = document.createElement('label');
-        label.for = categories[i];
-        label.textContent = categories[i];
+        label.for = "name";
+        label.textContent = "Name:";
 
         let input = document.createElement('input');
         input.type = 'text';
-        input.name = categories[i];
-
-        body.appendChild(line_break);
+        input.name = "name";
+        input.id = "name";
+        input.value = name;
+    
+        body.appendChild(label);
+        body.appendChild(document.createElement('br'));
         body.appendChild(input);
-    }
+        body.appendChild(document.createElement('br'));
+        body.appendChild(document.createElement('br'));
 
+        let label2 = document.createElement('label');
+        label2.for = "email";
+        label2.textContent = "Email:";
+
+        let input2 = document.createElement('input');
+        input2.type = 'text';
+        input2.name = "email";
+        input2.id = "email";
+        input2.value = email;
+    
+        body.appendChild(label2);
+        body.appendChild(document.createElement('br'));
+        body.appendChild(input2);
+        body.appendChild(document.createElement('br'));
+        body.appendChild(document.createElement('br'));
+    
+    body.appendChild(document.createElement('br'));
     return body;
 }
 
-let mod = createModal('hello', 'hello', 'hello')
-let htmlbody = document.querySelector('body');
-htmlbody.insertBefore(mod, document.querySelector('#first-child'));
-
 /* 
-            <div class="modal-body">
-              <label for="student">Name:</label>
-              <br>
-              <input type="text" name="student">
-              <br>
-              <br>
-              <label for="email">Email:</label>
-              <br>
-              <input type="text" name="email">
-              <br>
-              <br>
               <label for="grade-field">Grade:</label>
               <form name="grade-field" id="grade-field">
                 <input type="radio" value="1" id="first" name="grade" checked>
@@ -70,5 +91,4 @@ htmlbody.insertBefore(mod, document.querySelector('#first-child'));
                 <input class="left-space" type="radio" value="4" id="fourth" name="grade">
                 <label for="fourth">Fourth</label>
               </form>
-              <br>
-            </div> */
+*/
