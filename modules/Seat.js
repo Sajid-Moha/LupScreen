134760acc0509a5e08c9765e0a45fe1d1a446070
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-20 11:15:59 -0700

    Improved Cell Styling Empty cells have .5 opacity Seperated logic that condenses name & email into seperate functions. Name & email are both partially visible now when possible.

--- modules/Seat.js
+++ modules/Seat.js
@@ -55,15 +55,53 @@ function UpdateSeat(row, col) {
     if (name_input.value === '' && email_input.value === '') {
         cur_cell.classList.remove('taken');
         cur_cell.textContent = `${LETTERS[row]}-${col + 1}`;
-    } else if (name_input.value === '') {
-        cur_cell.classList.add('taken');
-        console.log(SEAT_GRID[row][col].email)
-        // if email is too long, we have to trim it
-        if (SEAT_GRID[row][col].email.length > 5) {
-            cur_cell.textContent = SEAT_GRID[row][col].email.substring(0, 5) + '\u2026';
-        } else { cur_cell.textContent = SEAT_GRID[row][col].email; }
-    } else {
-        cur_cell.classList.add('taken');
-        cur_cell.textContent = SEAT_GRID[row][col].name;
     }
+    else if (name_input.value === '') {
+        cur_cell.classList.add('taken');
+        // if email is too long, we have to trim it
+        let email = EmailCondenser(SEAT_GRID[row][col].email);
+        cur_cell.textContent = email;
+    }
+    else if (email_input.value === '') {
+        cur_cell.classList.add('taken');
+        let name = NameCondenser(SEAT_GRID[row][col].name);
+        cur_cell.textContent = name;
+    }
+    else {
+        cur_cell.classList.add('taken');
+        let name = NameCondenser(SEAT_GRID[row][col].name);
+        let email = EmailCondenser(SEAT_GRID[row][col].email);
+
+        cur_cell.innerHTML = `
+        ${name}
+        <br>
+        ${email}
+        `;
+    }
+}
+
+function NameCondenser(user_name) {
+    let name = '';
+    // if name is too long, we have to trim it
+    if (user_name.length > 5) {
+        name = user_name.substring(0, 5) + '\u2026';
+    } else {
+        name = user_name;
+    }
+
+    return name;
+}
+
+function EmailCondenser(email_address) {
+    // if email name is too long, we have to trim it
+    let email = '';
+    let email_name = email_address.split('@')[0];
+
+    if (email_name.length > 4) {
+        email = email_name.substring(0, 4) + '\u2026@\u2026';
+    } else {
+        email = email_name + '@\u2026';
+    }
+
+    return email;
 }
