47bc07ab7361315b819e0ac7fe47291fd992c50d
Sajid Mohammad <sajidmoha03@gmail.com>
2022-09-20 21:56:46 -0700

    Added some secondary checks for input fields Used regex to verify that letter coordinate is alpha implemented an alert to notify if a given cell doesn't exist

--- modules/finder.js
+++ modules/finder.js
@@ -3,7 +3,14 @@ let col_num = document.querySelector('#col-num');
 let search_btn = document.querySelector('#search-btn');
 
 search_btn.addEventListener('click', () => {
+    if (row_letter.value.length != 1 || !/[a-zA-Z]/.test(row_letter.value)) {
+        return;
+    }
     let cell_id = `${row_letter.value}-${col_num.value}`;
     let cur_cell = document.getElementById(cell_id);
+    if (!cur_cell) {
+        alert('coordinate not in range!');
+        return;
+    }
     cur_cell.focus();
 });
